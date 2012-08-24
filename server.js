052fc11d33240b27a324c9deb361f49e99b30ace
David Kormushoff <kormie@gmail.com>
2012-08-23 22:52:52 -0500

    Minimum Viable Server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 var express = require("express");
+
+var app = express.createServer();
+
+app.get('/', function(request, response) {
+  response.sendfile(__dirname + "/public/index.html");
+});
+
+app.listen(8080);
