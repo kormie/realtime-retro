563aaacf009c3bb05315e64b35d80f74c05b174e
David Kormushoff <kormie@gmail.com>
2012-08-23 23:15:45 -0500

    Post to the server

--- server.js
+++ server.js
@@ -2,8 +2,26 @@ var express = require("express");
 
 var app = express.createServer();
 
+var index = __dirname + "/public/index.html"
+
 app.get('/', function(request, response) {
-  response.sendfile(__dirname + "/public/index.html");
+  response.sendfile(index);
 });
+
+app.post('/create', function(request, response){
+  var text = '';
+  request.setEncoding('utf8');
+  request.on('data', function(chunk){
+    text += chunk;
+  });
+  request.on('end', function(){
+    var data = createPost(text);
+  });
+  response.sendfile(index);
+});
+
+function createPost (text) {
+  console.log(text);
+}
 
 app.listen(8080);
